70c5f6fad42b634686b9facfe8501e0905d48db8
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-07-28 13:39:13 -0300

    Erro no codigo

--- app/dao/respostaDAO.py
+++ app/dao/respostaDAO.py
@@ -115,7 +115,7 @@ def read_resposta():
         for result in results:
             resposta = (result[0], result[1], result[2], result[3], result[4], result[5])
             respostas.append(resposta)
-            return respostas
+        return respostas
     except(Exception, Error) as error:
         return("Erro ao listar respostas:", error)
     finally:
