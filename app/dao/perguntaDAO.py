f3d98ae540081dab497092b61edfd574f0b6c11c
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-09-14 21:47:51 -0300

    Criei o metodo de cadastrar pergunta, irei fazer alguns testes por conta de ser uma tabela que tem chaves estrangeiras

---
+++ app/dao/perguntaDAO.py
@@ -0,0 +1,20 @@
+from psycopg2 import Error 
+from app.config.conexao import Conexao
+
+#cria um metodo de cadastro questionario
+def create_pergunta(textopergunta, alt1, alt2, alt3, alt4, resposta, idpergunta, idquestionario):
+    conexao = Conexao()
+    conn = conexao.connect()
+    try:
+        cursor = conn.cursor()
+        query = ("insert into pergunta (textopegunta, alt1, alt2, alt3, alt4, resposta, idpergunta, idquestionario) values({}, {},{}, {},{}, {},{}, {})".format(textopergunta, alt1, alt2, alt3, alt4, resposta, idpergunta, idquestionario))
+        cursor.execute(query)
+        conn.commit()
+
+        return('Cadastro feito com sucesso!')
+    except(Exception, Error) as error:
+        conn.rollback()
+        return('Erro ao inserir dados:', error)
+    finally:
+        cursor.close()
+        conexao.close_connection(conn)
