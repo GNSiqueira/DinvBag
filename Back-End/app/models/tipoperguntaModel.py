d71783ad8611b814c5cd71b1add99e2799f4ecbb
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-09-23 11:09:08 -0300

    alterando variaveis do tipoperguntaModel

--- Back-End/app/models/tipoperguntaModel.py
+++ Back-End/app/models/tipoperguntaModel.py
@@ -1,23 +1,16 @@
 class TipoPergunta:
-    def __init__(self, idtipopergunta, nometipopergunta, descriçãopergunta):
-        self._idtipopergunta = idtipopergunta
-        self._nometipopergunta = nometipopergunta
-        self._descriçãopergunta = descriçãopergunta
+    def __init__(self, id_tipo_pergunta, nome_tipo_pergunta):
+        self._id_tipo_pergunta = id_tipo_pergunta
+        self._nome_tipo_pergunta = nome_tipo_pergunta
 
-    #encapsulamento idtipopergunta
-    def get_idtipopergunta(self):
-        return self._idtipopergunta
-    def set_idtipopergunta(self, new_idtipopergunta):
-        self._idtipopergunta = new_idtipopergunta
+    #encapsulamento id_tipo_pergunta
+    def get_id_tipo_pergunta(self):
+        return self._id_tipo_pergunta
+    def set_id_tipo_pergunta(self, new_id_tipo_pergunta):
+        self._id_tipo_pergunta = new_id_tipo_pergunta
 
-    #encapsulamento nometipopergunta
-    def get_nometipopergunta(self):
-        return self._nometipopergunta
-    def set_nometipopergunta(self, new_nometipopergunta):
-        self._nometipopergunta = new_nometipopergunta
-
-    #encapsulamento descriçãopergunta
-    def get_descriçãopergunta(self):
-        return self._descriçãopergunta
-    def set_descriçãopergunta(self, new_descriçãopergunta):
-        self._descriçãopergunta = new_descriçãopergunta
+    #encapsulamento nome_tipo_pergunta
+    def get_nome_tipo_pergunta(self):
+        return self._nome_tipo_pergunta
+    def set_nome_tipo_pergunta(self, new_nome_tipo_pergunta):
+        self._nome_tipo_pergunta = new_nome_tipo_pergunta
