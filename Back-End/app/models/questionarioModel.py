a91245dc0ec1c634c5e2e1c3626e556fb7b6878d
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-09-23 10:58:40 -0300

    alterando variaveis do questionarioModel

--- Back-End/app/models/questionarioModel.py
+++ Back-End/app/models/questionarioModel.py
@@ -1,21 +1,26 @@
 class Questionario:
-    def __init__(self, idquestionario, estagio, acertos):
-        self._idquestionario = idquestionario
-        self._estagio = estagio
-        self._acertos = acertos
+    def __init__(self, id_questionario, fase_questionario, acertos_questionario, id_usuario):
+        self._id_questionario = id_questionario
+        self._estagio_questionario = fase_questionario
+        self._acertos_questionario = acertos_questionario
+        self._id_usuario = id_usuario
 
-    #encapsulamento idquestionario
-    def get_idquestionatio(self):
-        return self._idquestionario
+    #encapsulamento id_questionario
+    def get_id_questionatio(self):
+        return self._id_questionario
     
-    #encapsulamento estagio
-    def get_estagio(self):
-        return self._estagio
-    def set_estagio(self, new_estagio):
-        self._estagio = new_estagio
+    #encapsulamento fase_questionario
+    def get_fase_questionario(self):
+        return self._fase_questionario
+    def set_fase_questionario(self, new_fase_questionario):
+        self._fase_questionario = new_fase_questionario
 
-    #encapsulamento acertos
-    def get_acertos(self):
-        return self._acertos
-    def set_acertos(self, new_acertos):
-        self._acertos = new_acertos
+    #encapsulamento acertos_questionario
+    def get_acertos_questionario(self):
+        return self._acertos_questionario
+    def set_acertos_questionario(self, new_acertos_questionario):
+        self._acertos_questionario = new_acertos_questionario
+
+    #encapsulamento id_usuario
+    def get_id_usuario(self):
+        return self._id_usuario
