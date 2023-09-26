864983d6c4e597afc56cceecaa1297da0ecc6aa0
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-09-26 15:04:51 -0300

    Alterações nos codigos sql na tabela Resposta

--- Back-End/app/dao/respostaDAO.py
+++ Back-End/app/dao/respostaDAO.py
@@ -2,12 +2,12 @@ from psycopg2 import Error
 from app.config.conexao import Conexao
 
 #create resposta
-def create_resposta(descricaoresposta, alternativaescolhida, datapergunta, idusuario, idperguta):
+def create_resposta(alternativa_respondida, alternativa_correta, datapergunta, idquestionario, idperguta):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("insert into resposta (descricaoresposta, alternativaescolhida, datapergunta, idusuario, idperguta) values ({}, {}, {}, {}, {})".format(descricaoresposta, alternativaescolhida, datapergunta, idusuario, idperguta))
+        query = ("insert into resposta (alternativa_respondida, alternativa_correta, data_pergunta, id_questionario, id_pergunta) values ({}, {}, {}, {}, {})".format(alternativa_respondida, alternativa_correta, datapergunta, idquestionario, idperguta))
         cursor.execute(query)
         conn.commit()
         return "Resposta adicionada com sucesso!"
@@ -18,49 +18,32 @@ def create_resposta(descricaoresposta, alternativaescolhida, datapergunta, idusu
         conexao.close_connection(conn)
         cursor.close()
 
-#update resposta(descricaoresposta)
-def update_resposta_descricao(id, descricao):
+#update resposta(alternativa_respondida)
+def update_resposta_alternativa_respondida(id, alternativa_respondida):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("update resposta set descricaoresposta = {} where idresposta = {}".format(id, descricao))
+        query = ("update resposta set alternativa_respondida = {} where id_resposta = {}".format(id, alternativa_respondida))
         cursor.execute(query)
         conn.commit()
-        return "Descrição alterada com sucesso!"
+        return "Alternativa respondida alterada com sucesso!"
     except(Exception, Error) as error:
-        return("Erro au atualizar informação:", error)
+        return("Erro ao atualizar informação:", error)
     finally:
         conexao.close_connection(conn)
         cursor.close()
 
-#update resposta(alternativaescolhida)
-def update_resposta_alternativa(id, alternativa):
+#update resposta(alternativa_correta)
+def update_resposta_alternativa_correta(id, alternativa_correta):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("update resposta set alternativaescolhida = {} where idresposta = {}".format(id, alternativa))
+        query = ("update resposta set alternativa_correta = {} where id_resposta = {}".format(id, alternativa_correta))
         cursor.execute(query)
         conn.commit()
-        return "Alternativa escolhica atualizada com sucesso!"
-
-    except(Exception, Error) as error:
-        return("Erro au atualizar informação:", error)
-    finally:
-        conexao.close_connection(conn)
-        cursor.close()
-
-#update resposta(datapergunta)
-def update_resposta_descricao(id, data):
-    conexao = Conexao()
-    conn = conexao.connect()
-    try:
-        cursor = conn.cursor()
-        query = ("update resposta set dataresposta = {} where idresposta = {}".format(id, data))
-        cursor.execute(query)
-        conn.commit()
-        return "Data da pergunta atualizada com sucesso!"
+        return "Alternativa correta atualizada com sucesso!"
 
     except(Exception, Error) as error:
         return("Erro au atualizar informação:", error)
@@ -68,16 +51,16 @@ def update_resposta_descricao(id, data):
         conexao.close_connection(conn)
         cursor.close()
 
-#update resposta(idusuario)
-def update_resposta_idusuario(id, idusuario):
+#update resposta(data_resposta)
+def update_resposta_data_resposta(id, data_resposta):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("update resposta set idusuario = {} where idresposta = {}".format(id, idusuario))
+        query = ("update resposta set data_resposta = {} where id_resposta = {}".format(id, data_resposta))
         cursor.execute(query)
         conn.commit()
-        return "IdUsuario atualizado com sucesso!"
+        return "Data da resposta atualizada com sucesso!"
 
     except(Exception, Error) as error:
         return("Erro au atualizar informação:", error)
@@ -85,13 +68,30 @@ def update_resposta_idusuario(id, idusuario):
         conexao.close_connection(conn)
         cursor.close()
 
-#update resposta(idpergunta)
-def update_resposta_idpergunta(id, idpergunta):
+#update resposta(id_questionario)
+def update_resposta_id_questionario(id, id_questionario):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("update resposta set idpergunta = {} where idresposta = {}".format(id, idpergunta))
+        query = ("update resposta set id_questionario = {} where id_resposta = {}".format(id, id_questionario))
+        cursor.execute(query)
+        conn.commit()
+        return "IdQuestionario atualizado com sucesso!"
+
+    except(Exception, Error) as error:
+        return("Erro au atualizar informação:", error)
+    finally:
+        conexao.close_connection(conn)
+        cursor.close()
+
+#update resposta(id_pergunta)
+def update_resposta_id_pergunta(id, idpergunta):
+    conexao = Conexao()
+    conn = conexao.connect()
+    try:
+        cursor = conn.cursor()
+        query = ("update resposta set id_pergunta = {} where id_resposta = {}".format(id, idpergunta))
         cursor.execute(query)
         conn.commit()
         return "IdPergunta atualizada com sucesso!"
@@ -128,7 +128,7 @@ def delet_resposta(id):
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("delete from resposta where idresposta = {}".format(id))
+        query = ("delete from resposta where id_resposta = {}".format(id))
         cursor.execute(query)
         conn.commit()
         return "Resposta deletada com sucesso!"
