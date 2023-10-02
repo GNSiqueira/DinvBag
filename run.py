04c2958ab3101904b726ec76611ad7822b10484f
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-10-02 13:12:10 -0300

    upar para servidor

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
 
-if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
+if __name__ == "main":
+    port = int(os.getenv('PORT'), '8000')
+
+    app.run(host='0.0.0.0', port = port)
