f239aaf73b57ec40eab10386ac5b91f37b80f7cb
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-10-06 14:33:49 -0300

    conexao com o banco de dados atualizada

--- app/config/conexao.py
+++ app/config/conexao.py
@@ -1,16 +1,18 @@
 from psycopg2 import connect, Error
+import os, psycopg2
 
 class Conexao:
     def connect(self):
         try:
-            conn = connect(
-                host="containers-us-west-108.railway.app",
-                port="7671",
-                database="railway",
-                user="postgres",
-                password="xw01F5KQ0vEbK3ubaUog"
-            )
+            host = "containers-us-west-108.railway.app",
+            port = "7671",
+            database = "railway",
+            user = "postgres",
+            password = "xw01F5KQ0vEbK3ubaUog"
+            database_url = ("postgresql://{}:{}@{}:{}/{}".format(user, password, host, port, database)) 
+            conn = psycopg2.connect(database_url)
             return conn
+        
         except (Exception, Error) as error:
             return("Erro ao conectar no banco:", error)
 
