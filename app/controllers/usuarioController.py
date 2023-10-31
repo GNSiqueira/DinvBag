25aa6a2e6c71ad44bffac05f32f33aa55563b9b0
GabrielNetoSiqueira <gabriel272173siqueira@gmail.com>
2023-10-31 15:00:03 -0300

    Alterações feitas para cookies, como armazenamento de id_usuario.

--- app/controllers/usuarioController.py
+++ app/controllers/usuarioController.py
@@ -2,6 +2,12 @@ from app.models.usuarioModel import Usuario
 from app.dao import usuarioDAO
 from flask import request, render_template, make_response
 
+def para_string(var):
+    return str(var)
+
+def para_int(var):
+    return int(var)
+
 #metodo de cadastro
 def cadastra():
     nome = request.form['nome']
@@ -20,9 +26,9 @@ def cadastra():
             user = Usuario(1, nome, email, senha, 'Inicio')
             inform = [user.get_nome_usuario(), user.get_email_usuario(), user.get_senha_usuario(), user.get_progresso_usuario()]
             resposta = usuarioDAO.create_usuario(inform)
-            id_usuario = usuarioDAO.pesquisar_usuario(email)
-            cookies = make_response()
-            cookies.set_cookie('id_usuario', id_usuario)
+            cookies = make_response(render_template('cadastro.html', static = 'app/static', resposta = resposta))
+            id_user = usuarioDAO.pesquisar_usuario(email)
+            cookies.set_cookie("iduser", para_string(id_user))
             return resposta
         
 #método de login      
@@ -33,11 +39,12 @@ def loga():
     senha_1 = usuarioDAO.verificar_usuario_senha(login) 
     if login_1 == login:
         if senha_1 == senha:
-            resposta = 'Usuario entra'
-            id_usuario = usuarioDAO.pesquisar_usuario(login)
-            cookies = make_response()
-            cookies.set_cookie('id_usuario', id_usuario)
-            return resposta
+            resposta = 'Usuario entra!'
+            cookies = make_response(render_template('loginprincipal.html', static = 'app/static', resposta = resposta))
+            id_user = usuarioDAO.pesquisar_usuario(login)
+            cookies.set_cookie("iduser", para_string(id_user))
+            return cookies
+        
         else:
             resposta = 'Usuario ou senha invalida'
             return resposta
