13bed30250bec277b01faa7b2422a541530d0537
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2023-11-28 10:55:18 -0300

    Crud de pergunta

--- app/controllers/perguntaController.py
+++ app/controllers/perguntaController.py
@@ -24,3 +24,57 @@ def carregar_questionario_tesouro():
             retorno['alter{}_p{}'.format(cc, pergunta)] = resp[cccc]
             cccc+=1
     return retorno
+
+def read_pergunta():
+    results = perguntaDAO.read_pergunta()
+    perguntas = []
+    for result in results:
+        pergunta = Pergunta(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7])
+        perguntas.append(pergunta)
+        
+    return perguntas
+
+def read_pergunta_um():    
+    ab = request.args['id-pergunta']
+    ass = []
+    results = perguntaDAO.read_pergunta_um(ab)
+    for result in results:
+        a = Pergunta(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7])
+        ass.append(a)
+    return ass
+
+def cadastrar_pergunta():
+    a = request.form['texto-pergunta']
+    b = request.form['alternativa1']
+    c = request.form['alternativa2']
+    d = request.form['alternativa3']
+    e = request.form['alternativa4']
+    f = request.form['resposta-pergunta']
+    g = request.form['id-tipo-pergunta']
+    classe = Pergunta(None, a, b, c, d, e, f, g)
+    h = perguntaDAO.verificar_pergunta(inform = [classe.get_texto_pergunta(), classe.get_alternativa1(), classe.get_alternativa2(), classe.get_alternativa3(), classe.get_alternativa4()])
+    print (h)
+    if h == []:
+       i =  perguntaDAO.create_pergunta(inform = [classe.get_texto_pergunta(), classe.get_alternativa1(), classe.get_alternativa2(), classe.get_alternativa3(), classe.get_alternativa4(), classe.get_resposta_pergunta(), classe.get_id_tipo_pergunta()])
+       print (i)
+    
+    pass
+
+def editar_pergunta():
+    h = request.form['id-pergunta']
+    a = request.form['texto-pergunta']
+    b = request.form['alternativa1']
+    c = request.form['alternativa2']
+    d = request.form['alternativa3']
+    e = request.form['alternativa4']
+    f = request.form['resposta-pergunta']
+    g = request.form['id-tipo-pergunta']
+    classe = Pergunta(h, a, b, c, d, e, f, g)
+    i = perguntaDAO.update_pergunta(inform = [classe.get_texto_pergunta(), classe.get_alternativa1(), classe.get_alternativa2(), classe.get_alternativa3(), classe.get_alternativa4(), classe.get_resposta_pergunta(), classe.get_id_tipo_pergunta(), classe.get_id_pergunta()])
+    pass
+
+def deletar_pergunta():
+    if request.method == "POST":
+        a = request.form['id-pergunta']
+        perguntaDAO.delete_pergunta(a)
+    pass 
--- app/dao/perguntaDAO.py
+++ app/dao/perguntaDAO.py
@@ -2,13 +2,13 @@ from psycopg2 import Error
 from app.config.conexao import Conexao
 
 #cria um metodo de cadastro questionario
-def create_pergunta(textopergunta, alt1, alt2, alt3, alt4, resposta, idtipopergunta):
+def create_pergunta(inform):
     conexao = Conexao()
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("insert into pergunta (texto_pergunta, alternativa1, alternativa2, alternativa3, alternativa4, resposta_pergunta, id_tipo_pergunta) values ('{}','{}','{}','{}','{}','{}',{});".format(textopergunta, alt1, alt2, alt3, alt4, resposta, idtipopergunta))
-        cursor.execute(query)
+        query = "insert into pergunta (texto_pergunta, alternativa1, alternativa2, alternativa3, alternativa4, resposta_pergunta, id_tipo_pergunta) values (%s,%s,%s,%s,%s,%s,%s);"
+        cursor.execute(query, inform)
         conn.commit()
 
         return('Cadastro feito com sucesso!')
@@ -178,7 +178,7 @@ def selecionar_perguntas(tipo):
     conn = conexao.connect()
     try:
         cursor = conn.cursor()
-        query = ("select p.* from pergunta p inner join tipo_pergunta t on t.id_tipo_pergunta = p.id_tipo_pergunta where tipo_pergunta = '{}';".format(tipo))
+        query = ("select p.* from pergunta p inner join tipo_pergunta t on t.id_tipo_pergunta = p.id_tipo_pergunta where tipo_pergunta = {};".format(tipo))
         cursor.execute(query)
         results = cursor.fetchall()
         conn.commit()
@@ -186,5 +186,59 @@ def selecionar_perguntas(tipo):
     except(Exception, Error) as error:
         return('Erro no codigo do banco: ', error)
     finally:
+        cursor.close()
+        conexao.close_connection(conn)
+
+def read_pergunta_um(id):
+    conexao = Conexao()
+    conn = conexao.connect()
+    try:
+        cursor = conn.cursor()
+        query = ("select * from pergunta where id_pergunta = {};".format(id))
+        cursor.execute(query)
+        results = cursor.fetchall()
+        perguntas = []
+        for result in results:
+            pergunta = (result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7])
+            perguntas.append(pergunta)
+        return perguntas
+    except(Exception, Error) as error:
+        return('Erro ao carregar pergunta: ', error)
+    finally: 
+        cursor.close()
+        conexao.close_connection(conn)
+
+def verificar_pergunta(inform):
+    conexao = Conexao()
+    conn = conexao.connect()
+    try:
+        cursor = conn.cursor()
+        query = ("select * from pergunta where texto_pergunta = %s and alternativa1 = %s and alternativa2 = %s and alternativa3 = %s and alternativa4 = %s;")
+        cursor.execute(query, inform)
+        results = cursor.fetchall()
+        perguntas = []
+        for result in results:
+            pergunta = (result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7])
+            perguntas.append(pergunta)
+        return results
+    except(Exception, Error) as error:
+        return('Erro ao carregar pergunta: ', error)
+    finally: 
+        cursor.close()
+        conexao.close_connection(conn)
+
+def update_pergunta(inform):
+    conexao = Conexao()
+    conn = conexao.connect()
+    try:
+        cursor = conn.cursor()
+        query = "update pergunta set texto_pergunta = %s, alternativa1 = %s, alternativa2 = %s, alternativa3 = %s, alternativa4 = %s, resposta_pergunta = %s, id_tipo_pergunta = %s where id_pergunta = %s;"
+        cursor.execute(query, inform)
+        conn.commit()
+
+        return ('Uptate feito com sucesso!')
+    except(Exception, Error) as error:
+        return('Erro ao carregar pergunta: ', error)
+    finally: 
         cursor.close()
         conexao.close_connection(conn)
